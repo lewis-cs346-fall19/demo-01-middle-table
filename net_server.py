80ea10ca434b0a40fd87e0389e2b02ccb9779ecb
Graham Walker <grahamwalker@dhcp-10-132-163-161.uawifi.arizona.edu>
2019-08-29 11:05:39 -0700

    second server commit

--- net_server.py
+++ net_server.py
@@ -1,1 +1,23 @@
 #server file
+
+def main():
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	
+	addr = ("1.2.3.4", 24611)
+	sock.bind(addr)
+
+	sock.listen(5)
+	for i in range (1,101):
+		(connectedSock, clientAddress) = sock.accept()
+	
+	try:
+		msg = sock.recv(1024).decode()
+	except:
+		sock.close()
+	
+	msg.append(msg[0] + "hello world")
+
+	sock.sendall(msg.encode())
+
+main()
+		
