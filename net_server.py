08d8511779541e1e3264b10327adc9151a7c9c7c
Graham Walker <grahamwalker@dhcp-10-132-136-5.uawifi.arizona.edu>
2019-09-03 09:37:39 -0700

    test

--- net_server.py
+++ net_server.py
@@ -1,18 +1,21 @@
 #server file
-
+import socket
 def main():
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	
-	addr = ("1.2.3.4", 24611)
+	addr = ("0.0.0.0", 24611)
 	sock.bind(addr)
 
 	sock.listen(5)
-	for i in range (1,101):
+	for i in range (5):
+		print("boobobobobo")
 		(connectedSock, clientAddress) = sock.accept()
-	
+	print(clientAddress)	
+
 	try:
 		msg = sock.recv(1024).decode()
 	except:
+		print("Something bad happened")
 		sock.close()
 	
 	msg.append(msg[0] + "hello world")
