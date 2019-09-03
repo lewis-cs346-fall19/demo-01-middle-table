0322a210e31979202e2e9bf056e9b2c79d420fdd
mcheffer <mcheffer@email.arizona.edu>
2019-09-03 14:45:16 -0700

    end of class 9/3

--- net_client.py
+++ net_client.py
@@ -10,9 +10,13 @@ def main():
     addr = (host, 24611)
 
     sock.connect(addr)
-
+    in_msg = sock.recv(1024)
+    print(in_msg)
     user = input()
     while(user.lower() != "n"):
-    	sock.sendall(user.encode('utf-8'))
-    	sock.recv(1024)
+        sock.sendall(user.encode('utf-8'))
+        in_msg = sock.recv(1024)
+        print(in_msg)
+        user = input()
+    sock.close()
 main()
--- net_server.py
+++ net_server.py
@@ -1,27 +1,23 @@
 #server file
 import socket
 def main():
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	host = socket.gethostname()
-	addr = (host, 24611)
-	sock.bind(addr)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = socket.gethostname()
+    addr = (host, 24611)
+    sock.bind(addr)
 
-	sock.listen(5)
-	while True:
-		connectedSock, clientAddress = sock.accept()
-		print("Got a connection from %s" % str(addr))
-		msg = 'Thank you for connecting'+ "\r\n"
-		connectedSock.send(msg.encode('ascii'))
-	sock.close()	
-	
-	# try:
-	# 	msg = sock.recv(1024).decode()
-	# except:
-	# 	sock.close()
-	
-	# msg.append(msg[0] + "hello world")
-
-	# sock.sendall(msg.encode())
+    sock.listen(5)
+    connectedSock, clientAddress = sock.accept()
+    print("Got a connection from %s" % str(addr))
+    msg = 'Thank you for connecting'+ "\n"
+    connectedSock.send(msg.encode('ascii'))
+    while True:
+        try:
+            msg = connectedSock.recv(1024).decode()
+            msg = msg[::-1]
+            connectedSock.send(msg.encode('ascii'))
+        except:
+            sock.close()
 
 main()
 		
