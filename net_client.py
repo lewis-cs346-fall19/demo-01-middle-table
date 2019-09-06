e08409beff8e2eae425847de3b9a605236782337
mcheffer <mcheffer@email.arizona.edu>
2019-09-06 09:37:42 -0700

    more comments

--- net_client.py
+++ net_client.py
@@ -5,18 +5,28 @@
 import socket
 
 def main():
+
+    #create socket object
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    #returns host name
     host = socket.gethostname()
+
     addr = (host, 24611)
 
+    #connect to address
     sock.connect(addr)
+
+    #accept message from server
     in_msg = sock.recv(1024)
     print(str(in_msg))
-    user = input()
-    while(user.lower() != "n"):
+
+    #get user input
+    user = input("Enter a string or '1' to quit: ")
+    while(user.lower() != "1"):
         sock.sendall(user.encode('utf-8'))
-        in_msg = sock.recv(1024)
-        print(in_msg)
-        user = input()
-    sock.close()
+        in_msg = str(sock.recv(1024))
+        print(in_msg[2:])
+        user = input("Enter a string or '1' to quit: ")
+    sock.close()    
 main()
--- net_server.py
+++ net_server.py
@@ -1,23 +1,44 @@
 #server file
 import socket
+
 def main():
+    #create socket object
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    #returns name of local machine
     host = socket.gethostname()
+
     addr = (host, 24611)
+
+    #binds socket to port 24611
     sock.bind(addr)
 
+    #listen for connection, queue 5
     sock.listen(5)
-    connectedSock, clientAddress = sock.accept()
-    print("Connected with %s\n" % str(addr))
-    msg = "Connected\n"
-    connectedSock.send(msg.encode('ascii'))
+
     while True:
+        #accept connection
+        (connectedSock, clientAddress) = sock.accept()
+        print("Connected with %s" % addr[0])
+        msg = "Connected"
+        connectedSock.send(msg.encode('utf-8'))
         try:
-            msg = connectedSock.recv(1024).decode()
-            msg = msg[::-1]
-            connectedSock.send(msg.encode('ascii'))
+            while True:
+
+                #receive message
+                msg = connectedSock.recv(1024).decode()
+
+                if not msg:
+                    break
+
+                else:
+                    #reverse and return message
+                    msg = msg[::-1]
+                    connectedSock.sendall(msg.encode('utf-8'))
         except:
             sock.close()
-
+        connectedSock.close()
+        print("Connection with %s ended\n." % addr[0])
+        
 main()
 		
